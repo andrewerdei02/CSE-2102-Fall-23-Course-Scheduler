a235d9c86ee503ddf722ac6122886676e296194f
jackallen123 <128607553+jackallen123@users.noreply.github.com>
2023-10-03 14:51:06 -0400

    Add days of week to top of web app

--- frontend/src/components/WeeklyCalendar.vue
+++ frontend/src/components/WeeklyCalendar.vue
@@ -1,5 +1,13 @@
 <template>
-<h1>Hello, world!</h1>
+    <div>
+        <div class="days-of-week">
+            <div class="day-of-week">Monday</div>
+            <div class="day-of-week">Tuesday</div>
+            <div class="day-of-week">Wednesday</div>
+            <div class="day-of-week">Thursday</div>
+            <div class="day-of-week">Friday</div>
+        </div>
+    </div>
 
 </template>
 
@@ -8,5 +16,17 @@
 </script>
 
 <style>
-/* Add style here */
+    .days-of-week {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-left: 200px;
+        margin-right: 200px;
+        margin-top: 100px;
+    }
+
+    .day-of-week {
+        font-size: 24px;
+        font-weight: bold;
+    }
 </style>
