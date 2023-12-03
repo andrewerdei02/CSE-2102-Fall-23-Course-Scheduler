4cf46fea9deae0079ed489db0efad32a39d5f7cf
Crina Gutu <crina.gutu@gmail.com>
2023-12-03 14:08:35 -0500

    Updated to fix search functionality

--- frontend/src/components/ClassSearch.vue
+++ frontend/src/components/ClassSearch.vue
@@ -3,10 +3,60 @@
         <div class="course-selector">
             <h2 class="title">Course Selector</h2>
             <input type="text" v-model="search" placeholder="Search for a course" class="search-bar"/>
+        <table border="1px">
+            <tr>
+                <td>Class Name</td>
+                <td>Professor</td>
+            </tr>
+            <tr v-for="item in filteredClasses" :key="item.className" @click="showCourseDetails(item)" style="cursor: pointer;">
+                <td>{{ item.className }}</td>
+                <td>{{ item.professor }}</td>
+            </tr>
+        </table>
+
+        <div v-if="selectedCourse">
+            <h3>Selected Course Details</h3>
+            <p><strong>Class Name:</strong> {{ selectedCourse.className }}</p>
+            <p><strong>Professor:</strong> {{ selectedCourse.professor }}</p>
+            <p>More Info</p>
+            <!-- Add more details as needed -->
+        </div>
         </div>
     </div>
-  </template>
+</template>
+
   <script>
+   export default{
+    name: 'ClassSearch',
+    data(){
+        return {
+            post: [],
+            search: '',
+            selectedCourse: null
+        }
+    },
+    methods: {
+      async getResult(){
+        const response = await fetch('https://rxyqf233x8.execute-api.us-east-1.amazonaws.com/prod2/classes')
+        const data = await response.json()
+        this.post = data.classes
+        console.log(data.classes)
+      },
+      showCourseDetails(course) {
+        this.selectedCourse = course;
+      },
+    },
+    mounted(){
+        this.getResult()
+    },
+    computed: {
+        filteredClasses() {
+        const query = this.search.toLowerCase();
+        return this.post.filter(
+            (post) => post.className.toLowerCase().includes(query)); 
+    }
+  }
+   }
   </script>
   
 <style >
